1756dc43927f85fe51c618c35be23e1d986ae508
yameste <yameste@yahoo.co.uk>
2016-12-12 09:55:25 +0000

    added image optimisation

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,7 +4,8 @@
     <div class="container">
         <div class="middle-logo">
             <img class="logo"
-                 src="/images/logo_medium.png"
+                 srcset="/images/logo_medium.png 1080w,
+                        /images/logo_small.png 760w"
                  alt="The ormrepo thoughtful logo">
         </div>
         <!-- /.middle-logo -->
