27aff294302096d071e50af908b3da365faaabeb
yameste <yameste@yahoo.co.uk>
2016-12-18 12:55:08 +0000

    added rss controller

--- app/Http/Controllers/RssController.php
+++ app/Http/Controllers/RssController.php
@@ -16,28 +16,39 @@ class RssController extends Controller
     public function generate()
     {
         try {
+
             $blogs = Blog::latest()->published()->take(10)->get();
 
-            $feed = \App::make('feed');
-            $feed->title = 'Ormblog';
-            $feed->description = 'This is my blog about this';
-            // $feed->logo = asset('img/logo.png'); //optional
-            $feed->link = url('feed');
-            $feed->setDateFormat('datetime'); // 'datetime', 'timestamp' or 'carbon'
-            $feed->pubdate = $blogs[0]->created_at;
-            $feed->lang = 'en';
-            $feed->setShortening(true); // true or false
-            $feed->setTextLimit(100); // maximum length of description text
+            if ($blogs->isEmpty()) {
 
-            foreach ($blogs as $blog) {
-                // set item's title, author, url, pubdate, description and content
-                $feed->add($blog->title, 'Author', url('blogs/' . $blog->id), $blog->created_at, $blog->content, $blog->content);
+                return 'no RSS feeds today';
+
+            } else {
+
+                $feed = \App::make('feed');
+                $feed->title = 'Ormrepo';
+                $feed->description = 'This is my blog about this';
+                // $feed->logo = asset('img/logo.png'); //optional
+                $feed->link = url('feed');
+                $feed->setDateFormat('datetime'); // 'datetime', 'timestamp' or 'carbon'
+                $feed->pubdate = $blogs[0]->created_at;
+                $feed->lang = 'en';
+                $feed->setShortening(true); // true or false
+                $feed->setTextLimit(100); // maximum length of description text
+
+
+                foreach ($blogs as $blog) {
+                    // set item's title, author, url, pubdate, description and content
+                    $feed->add($blog->title, 'Author', url('blogs/' . $blog->id), $blog->created_at, $blog->content, $blog->content);
+                }
             }
+
         } catch (\Exception $e) {
 
-            throw new RSSNotFoundException($e->getMessage());
+                throw new RSSNotFoundException($e->getMessage());
         }
 
-        return $feed->render('rss'); // or atom
+                return $feed->render('rss'); // or atom
+        }
+
     }
-}
