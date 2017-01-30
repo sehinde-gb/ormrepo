a2afe7e43d98ddb0f343874b92be901cce002773
yameste <yameste@yahoo.co.uk>
2017-01-30 10:12:33 +0000

    removed categories migration temporary

--- database/migrations/2016_10_12_160655_create_blogs_table.php
+++ database/migrations/2016_10_12_160655_create_blogs_table.php
@@ -15,7 +15,7 @@
             Schema::create('blogs', function(Blueprint $table) {
                 $table->increments('id');
                 $table->integer('user_id')->unsigned()->index();
-                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->unsigned();
                 $table->string('title');
                 $table->string('excerpt');
                 $table->binary('feat_image');
