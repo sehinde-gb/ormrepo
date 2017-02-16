10fe34025ce47e95ad31a4415a8f1615eea431ef
yameste <yameste@yahoo.co.uk>
2017-02-16 08:27:14 +0000

    added notification parameters to blog published notification

--- app/Notifications/BlogPublished.php
+++ app/Notifications/BlogPublished.php
@@ -28,6 +28,10 @@ class BlogPublished extends Notification implements ShouldQueue
         return [OneSignalChannel::class];
     }
 
+    /**
+     * @param $notifiable
+     * @return $this
+     */
     public function toOneSignal($notifiable)
     {
         return OneSignalMessage::create()
@@ -40,6 +44,23 @@ class BlogPublished extends Notification implements ShouldQueue
                     ->icon('https://ormrepo.co.uk/images/ormrepo-tiny.png')
                     ->url('http://ormrepo.co.uk')
             );
+    }
+
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'user_id' => $this->user->id,
+            'name' => $this->user->name,
+            'username' => $this->user->username,
+            'email' =>$this->user->email
+        ];
     }
 }
 
