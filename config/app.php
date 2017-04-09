0a1d97327945ce641fb705d19d809d3c8e0ba852
yameste <yameste@yahoo.co.uk>
2017-04-09 11:14:55 +0100

    ran compile

--- config/app.php
+++ config/app.php
@@ -183,7 +183,7 @@ return [
         //App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Laravel\Tinker\TinkerServiceProvider::class,
+        //Laravel\Tinker\TinkerServiceProvider::class,
 
     ],
 
