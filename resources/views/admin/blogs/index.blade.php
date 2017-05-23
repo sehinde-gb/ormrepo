651cb98a96e62ffc2588783f879e285c28313630
yameste <yameste@yahoo.co.uk>
2017-05-23 10:26:33 +0100

    removed spatie newsletter because there was only single opt in now installing mailchimp embedded form this has double optin

--- resources/views/admin/blogs/index.blade.php
+++ resources/views/admin/blogs/index.blade.php
@@ -147,9 +147,19 @@
                 <h3 class="news--heading">Sign Up To Receive Our Updates</h3>
                 <h4 class="lorem--text">You consent to receiving bi-annual email updates we wont burden you with spam.</h4>
                 <div class="form-group">
-                    {!! Form::open(array('action' => 'SubscribeUserController@store', 'class' => 'form-group', 'placeholder'=>'Your email'))  !!}
-                    {!! Form::text('email', null, [ 'id' => 'box', 'class' => 'form-group', 'placeholder'=>' Your email']) !!}
-                    {!!  Form::submit('add', array('class' => 'btn-add')) !!}
+                    <div id="mc_embed_signup">
+                        <form action="//twitter.us9.list-manage.com/subscribe/post?u=1e496727dce7860147071555a&amp;id=1c16b906b8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                            <div id="mc_embed_signup_scroll">
+
+                                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_1e496727dce7860147071555a_1c16b906b8" tabindex="-1" value=""></div>
+                                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <!--End mc_embed_signup-->
                 </div><!-- /.form-group -->
             </div><!-- /.subscribe -->
         </div><!-- /.newsletter-container -->
--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -146,9 +146,26 @@
                 <h3 class="news--heading">Sign Up To Receive Our Updates</h3>
                 <h4 class="lorem--text is--lorem-padding">You consent to receiving bi-annual email updates we wont burden you with spam.</h4>
                 <div class="form-group">
-                    {!! Form::open(array('action' => 'SubscriptionsController@store', 'class' => 'form-group', 'placeholder'=>'Your email'))  !!}
-                    {!! Form::text('email', null, [ 'id' => 'box', 'class' => 'form-group', 'placeholder'=>' Your email']) !!}
-                    {!!  Form::submit('add', array('class' => 'btn-add')) !!}
+                    <!-- Begin MailChimp Signup Form -->
+                    <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+                    <style type="text/css">
+                        #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+                        /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+                           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                    </style>
+                    <div id="mc_embed_signup">
+                        <form action="//twitter.us9.list-manage.com/subscribe/post?u=1e496727dce7860147071555a&amp;id=1c16b906b8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                            <div id="mc_embed_signup_scroll">
+
+                                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_1e496727dce7860147071555a_1c16b906b8" tabindex="-1" value=""></div>
+                                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <!--End mc_embed_signup-->
                 </div><!-- /.form-group -->
 
             </div><!-- /.subscribe -->
