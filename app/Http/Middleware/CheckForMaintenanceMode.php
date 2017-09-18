cd1fbe81fcba4bd791582d58724fa22ea1dcef14
yameste <yameste@yahoo.co.uk>
2017-09-18 08:10:48 +0100

    added check for maintenance middleware

---
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Routing\Middleware;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\Request;
+
+class CheckForMaintenanceMode implements Middleware {
+
+    protected $request;
+    protected $app;
+
+    public function __construct(Application $app, Request $request)
+    {
+        $this->app = $app;
+        $this->request = $request;
+    }
+
+    public function handle($request, Closure $next)
+    {
+        if ($this->app->isDownForMaintenance() &&
+            !in_array($this->request->getClientIp(), ['86.10.190.248', '86.4.7.24']))
+        {
+            return response('Be right back!', 503);
+        }
+
+        return $next($request);
+    }
+
+}
+
