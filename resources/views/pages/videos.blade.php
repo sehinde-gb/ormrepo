5b2c646699f9793b88ebd70d1361b15a2ddaa724
yameste <yameste@yahoo.co.uk>
2017-09-20 22:17:15 +0100

    removed video titles

--- resources/views/pages/videos.blade.php
+++ resources/views/pages/videos.blade.php
@@ -14,15 +14,13 @@
 
         <div class="composite-container">
             <div class="single-container">
-                <h4>Social Diversity Series</h4>
-                <p>A series that focuses on how some organisations have been slow to evolve around the cultural, racial and social strategies within the tech sector.</p>
+
                 <h5 class="is--black is--capitals">Episode 1: How can tech firms fortify their brands through cultural diversity</h5>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/_UHsWdhNk8c" frameborder="0" allowfullscreen></iframe>
             </div><!-- /.single-container -->
 
             <div class="single-container">
-                <h4>Mind Body & Soul Series</h4>
-                <p>We will delve in to how you can establish trust between you and your cohorts and we will showcase situations in which empathy is an important characteristic in order to form long lasting relationships.</p>
+
                 <h5 class="is--black is--capitals">Episode 2: How can we use mindfulness in our quest for creativity</h5>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/gczfKRNHCOs" frameborder="0" allowfullscreen></iframe>
             </div><!-- /.single-container -->
@@ -30,15 +28,13 @@
 
             <div class="composite-container">
                 <div class="single-container">
-                    <h4>Scaffolding the Front End Series</h4>
-                    <p>A series that showcases web development techniques using front end languages such as Javascript and Javascript frameworks such as Vuejs.</p>
+
                     <h5 class="is--black is--capitals">Episode 3: How can we use fonts responsively in mobile design</h5>
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/0jvz6l-xb-c" frameborder="0" allowfullscreen></iframe>
                 </div><!-- /.single-container -->
 
                 <div class="single-container">
-                    <h4>Social Diversity Series</h4>
-                    <p>A series that focuses on how some organisations have been slow to evolve around the cultural, racial and social strategies within the tech sector.</p>
+
                     <h5 class="is--black is--capitals">Episode 4: How Tech firms can use social mobility can enable opportunities for everyone instead of the privileged few</h5>
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/A2x8wM0vNHo" frameborder="0" allowfullscreen></iframe>
                 </div><!-- /.single-container -->
@@ -47,13 +43,12 @@
 
             <div class="composite-container">
                 <div class="single-container">
-                    <h4>Social Diversity Series</h4>
+
                     <h5 class="is--black is--capitals">Episode 5: How Can We Derive Value From Developer Inspired Meetup Groups?</h5>
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/rTVHSNLnhzY" frameborder="0" allowfullscreen></iframe>
                 </div><!-- /.single-container -->
 
                 <div class="single-container">
-                    <h4>Social Diversity Series</h4>
                     <h5 class="is--black is--capitals">Episode 6: How does implicit bias influence the cultural fit within tech firms? </h5>
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/VbsivYheqgY" frameborder="0" allowfullscreen></iframe>
                 </div><!-- /.single-container -->
