d1ebc0cdc3966635a27ac18b6a6434a473c4a49f
yameste <yameste@yahoo.co.uk>
2017-09-27 08:19:06 +0100

    removed hidden mobile

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -64,7 +64,7 @@
                                 </div><!-- /.column is-one-quarter is-dark-grey is-flex is-four-->
 
                             @elseif($blog->id == 3)
-                                <div class="column is-one-quarter box-is-beige is-flex is-five is-hidden-tablet-only is-hidden-mobile">
+                                <div class="column is-one-quarter box-is-beige is-flex is-five is-hidden-tablet-only">
                                     <div class="name-column">
                                         <h6 class="box--name">Karan Kanuga</h6>
                                         <h6 class="box--title">Project</h6>
