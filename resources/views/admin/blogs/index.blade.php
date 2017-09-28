0ef893d2d295a231edffc92b4e8799957f95e500
yameste <yameste@yahoo.co.uk>
2017-09-28 11:05:07 +0100

    amended h6

--- resources/views/admin/blogs/index.blade.php
+++ resources/views/admin/blogs/index.blade.php
@@ -70,7 +70,8 @@
                                 <div class="column is-one-quarter box-is-beige is-flex is-five">
                                     <div class="name-column">
                                         <h6 class="box--name">Karan Kanuga</h6>
-                                        <h6 class="box--title">Project Management & SEO</h6>
+                                        <h6 class="box--title">Project Management </h6>
+                                        <h6 class="box--title"> & SEO</h6>
                                     </div><!-- /.name-column -->
 
                                 </div><!-- /.column is-one-quarter box-is-beige is-flex is-five is-hidden-mobile-->
