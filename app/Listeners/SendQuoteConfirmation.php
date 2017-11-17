12cfb3c8ec069494c6194a0f6162d6b0a33a53cd
yameste <yameste@yahoo.co.uk>
2017-11-17 15:19:57 +0000

    merged upgrade

--- app/Listeners/SendQuoteConfirmation.php
+++ app/Listeners/SendQuoteConfirmation.php
@@ -30,8 +30,8 @@ class SendQuoteConfirmation implements ShouldQueue
         flash()->success('Feedback', 'We will provide a response within 24 hours');
 
         //$when = Carbon::now()->addMinutes(0);
-        //Mail::to('info@ormrepo.co.uk')->queue(new QuoteSent($event));
-        Mail::send('info@ormrepo.co.uk')->send(new QuoteSent($event));
+        Mail::to('info@ormrepo.co.uk')->queue(new QuoteSent($event));
+        //Mail::send('info@ormrepo.co.uk')->queue(new QuoteSent($event));
 
 
         //Mail::to('info@ormrepo.co.uk')
