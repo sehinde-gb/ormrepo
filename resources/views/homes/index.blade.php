d6d1c1f4a414f3ca758e537a7c532232db0ad8a8
yameste <yameste@yahoo.co.uk>
2017-12-01 10:42:08 +0000

    rectified opening hours error

--- resources/views/homes/index.blade.php
+++ resources/views/homes/index.blade.php
@@ -185,7 +185,7 @@
 
             <div class="contact-text">
                 <h5 class="is--beige text-transform">Email: info@ormrepo.co.uk</h5>
-                <h5 class="is--beige text-transform">Opening Hours: 9pm - 5pm</h5>
+                <h5 class="is--beige text-transform">Opening Hours: 9am - 5pm</h5>
                 <h5 class="is--beige text-transform">Telephone: 07379 829189</h5>
             </div><!-- /.text-container -->
 
