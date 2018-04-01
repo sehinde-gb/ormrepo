b98a381d6ae7dbd039eb1abbab3cf4eadc8bb3f5
yameste <yameste@yahoo.co.uk>
2018-04-01 10:21:53 +0100

    updated blog images

--- resources/views/admin/blogs/index.blade.php
+++ resources/views/admin/blogs/index.blade.php
@@ -358,6 +358,17 @@
                                         <h6><a href="{{ url('admin/blogs', $blog->id) }}">{{ $blog->title }}</a></h6>
                                     </div><!-- /.arrow -->
                                 </div><!-- /.column is-one-quarter is-grey is-flex is-thirty-three-->
+
+                            @elseif($blog->id == 30)
+                                <div class="column is-one-quarter is-grey is-flex is-thirty-four">
+                                    <div class="arrow">
+                                        <div class="indexed-image">
+                                            <a href="{{ url('admin/blogs', $blog->id) }}"><img src="{{ asset('featured/images/' . $blog->id. '.png') }}" class="reduction" alt="The uploaded featured image"></a>
+                                        </div>
+                                        <i class="fa fa-long-arrow-left fa-2x"></i>
+                                        <h6><a href="{{ url('admin/blogs', $blog->id) }}">{{ $blog->title }}</a></h6>
+                                    </div><!-- /.arrow -->
+                                </div><!-- /.column is-one-quarter is-grey is-flex is-thirty-four-->
                             @endif
 
                         @endforeach
--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -364,6 +364,17 @@
                                     </div><!-- /.arrow -->
                                 </div><!-- /.column is-one-quarter is-grey is-flex is-thirty-three-->
 
+                            @elseif($blog->id == 30)
+                                <div class="column is-one-quarter is-dark-grey is-flex is-thirty-four">
+                                    <div class="arrow">
+                                        <div class="indexed-image">
+                                            <a href="{{ url('blogs', $blog->id) }}"><img src="{{ asset('featured/images/' . $blog->id. '.png') }}" class="reduction" alt="The uploaded featured image"></a>
+                                        </div><!-- /.indexed-image -->
+                                        <i class="fa fa-long-arrow-left fa-2x"></i>
+                                        <h6><a href="{{ url('blogs', $blog->slug) }}">{{ $blog->title }}</a></h6>
+                                    </div><!-- /.arrow -->
+                                </div><!-- /.column is-one-quarter is-dark-grey is-flex is-thirty-four-->
+
                             @endif
 
                         @endforeach
