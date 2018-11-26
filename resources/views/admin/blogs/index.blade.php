49923658d2d9e279f56ccd04c187b1bb8d374646
yameste <yameste@yahoo.co.uk>
2018-11-26 08:59:27 +0000

    updated blog post

--- resources/views/admin/blogs/index.blade.php
+++ resources/views/admin/blogs/index.blade.php
@@ -563,7 +563,17 @@
                                         <i class="fa fa-long-arrow-left fa-2x"></i>
                                         <h6><a href="{{ url('admin/blogs', $blog->id) }}">{{ $blog->title }}</a></h6>
                                     </div><!-- /.arrow -->
-                                </div><!-- /.column is-one-quarter is-dark-grey is-flex is-fifty-three-->   
+                                </div><!-- /.column is-one-quarter is-dark-grey is-flex is-fifty-three-->
+                            @elseif($blog->id == 50)
+                                <div class="column is-one-quarter is-light-grey is-flex is-fifty-four">
+                                    <div class="arrow">
+                                        <div class="indexed-image">
+                                            <a href="{{ url('admin/blogs', $blog->id) }}"><img src="{{ asset('featured/images/' . $blog->id. '.png') }}" class="reduction" alt="Blockchain Infrastructure Contract "></a>
+                                        </div>
+                                        <i class="fa fa-long-arrow-left fa-2x"></i>
+                                        <h6><a href="{{ url('admin/blogs', $blog->id) }}">{{ $blog->title }}</a></h6>
+                                    </div><!-- /.arrow -->
+                                </div><!-- /.column is-one-quarter is-light-grey is-flex is-fifty-four-->          
 
                             @endif
 
