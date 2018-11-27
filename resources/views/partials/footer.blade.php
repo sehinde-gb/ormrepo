a529300c8a66925b607c7eb5fcedd7dc6be3cfac
yameste <yameste@yahoo.co.uk>
2018-11-27 15:50:07 +0000

    updated subscribe sign up form

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -16,27 +16,53 @@
                     <p class="is--pastel is--uppercase is--padded-l20 is--uppercase">Keep in touch</p>
                 </div><!-- /.footer-header -->
                         <h3 class="news--heading">Sign Up To Receive Our Updates</h3>
-                        <h4 class="lorem--text is--lorem-padding">You consent to receiving monthly email updates only, so you won't see anything from us in your junk mail.</h4>
+                        <h4 class="lorem--text">You consent to receiving monthly email updates only, so you won't see anything from us in your junk mail.</h4>
 
+<!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+    #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+    #mc_embed_signup label {color: white !important; font-size: 0.9em !important;}
 
-                    <!-- Begin MailChimp Signup Form -->
-                    <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-                    <style type="text/css">
-                        #mc_embed_signup{background:#444444; clear:left; font:14px Helvetica,Arial,sans-serif; width:50%;}
-                        /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-                           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-                    </style>
-                    <div id="mc_embed_signup">
-                        <form action="//twitter.us9.list-manage.com/subscribe/post?u=1e496727dce7860147071555a&amp;id=1c16b906b8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                            <div id="mc_embed_signup_scroll">
+	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<style type="text/css">
+	#mc-embedded-subscribe-form input[type=checkbox]{display: inline; width: auto;margin-right: 10px;}
+	#mergeRow-gdpr {margin-top: 20px;}
+	#mergeRow-gdpr fieldset label {font-weight: normal;}
+    #mc-embedded-subscribe-form .mc_fieldset{border:none;min-height: 0px;padding-bottom:0px; padding-left: 20px !important;}
+    
+</style>
 
-                                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-                                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_1e496727dce7860147071555a_1c16b906b8" tabindex="-1" value=""></div>
-                                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                            </div>
-                        </form>
-                    </div><!--End mc_embed_signup-->
+<div id="mc_embed_signup">
+<form action="https://ormrepo.us19.list-manage.com/subscribe/post?u=3c9ca0fce5f540adb6eefe51a&amp;id=883f50667f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<label for="mce-EMAIL">Subscribe to our mailing list</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+<div id="mergeRow-gdpr" class="mergeRow gdpr-mergeRow content__gdprBlock mc-field-group">
+    <div class="content__gdpr">
+        <label>Marketing Permissions</label>
+        <p class="lorem--text">Please select all the ways you would like to hear from Ormrepo:</p>
+        <fieldset class="mc_fieldset gdprRequired mc-field-group lorem--text name="interestgroup_field">
+		<label class="checkbox subfield" for="gdpr_17499"><input type="checkbox" id="gdpr_17499" name="gdpr[17499]" value="Y" class="av-checkbox "><span>Email</span> </label><label class="checkbox subfield" for="gdpr_17503"><input type="checkbox" id="gdpr_17503" name="gdpr[17503]" value="Y" class="av-checkbox "><span>Direct Mail</span> </label>
+        </fieldset>
+        <p class="lorem--text">You can unsubscribe at any time by clicking the link in the footer of our emails. For information about our privacy practices, please visit our website.</p>
+    </div>
+    <div class="content__gdprLegal">
+        <p class="lorem--text">We use Mailchimp as our marketing platform. By clicking below to subscribe, you acknowledge that your information will be transferred to Mailchimp for processing. <a href="https://mailchimp.com/legal/" target="_blank">Learn more about Mailchimp's privacy practices here.</a></p>
+    </div>
+</div>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3c9ca0fce5f540adb6eefe51a_883f50667f" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
 
 
                 <div class="icon-social">
