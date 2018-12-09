5838b7f7cddbcac8882b479867702f1eb7ded356
yameste <yameste@yahoo.co.uk>
2018-12-09 13:07:24 +0000

    updated blog

--- resources/views/admin/blogs/index.blade.php
+++ resources/views/admin/blogs/index.blade.php
@@ -603,7 +603,17 @@
                                         <i class="fa fa-long-arrow-left fa-2x"></i>
                                         <h6><a href="{{ url('admin/blogs', $blog->id) }}">{{ $blog->title }}</a></h6>
                                     </div><!-- /.arrow -->
-                                </div><!-- /.column is-one-quarter is-dark-grey is-flex is-fifty-seven-->     
+                                </div><!-- /.column is-one-quarter is-dark-grey is-flex is-fifty-seven--> 
+                            @elseif($blog->id == 54)
+                                <div class="column is-one-quarter is-grey is-flex is-fifty-eight">
+                                    <div class="arrow">
+                                        <div class="indexed-image">
+                                            <a href="{{ url('admin/blogs', $blog->id) }}"><img src="{{ asset('featured/images/' . $blog->id. '.png') }}" class="reduction" alt="Public Transport: Photo by Pexels "></a>
+                                        </div>
+                                        <i class="fa fa-long-arrow-left fa-2x"></i>
+                                        <h6><a href="{{ url('admin/blogs', $blog->id) }}">{{ $blog->title }}</a></h6>
+                                    </div><!-- /.arrow -->
+                                </div><!-- /.column is-one-quarter is-grey is-flex is-fifty-eight-->             
 
                             @endif
 
--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -604,7 +604,17 @@
                                         <i class="fa fa-long-arrow-left fa-2x"></i>
                                         <h6><a href="{{ url('blogs', $blog->slug) }}">{{ $blog->title }}</a></h6>
                                     </div><!-- /.arrow -->
-                                </div><!-- /.column is-one-quarter is-dark-grey is-flex is-fifty-seven-->                                                                                        
+                                </div><!-- /.column is-one-quarter is-dark-grey is-flex is-fifty-seven-->
+                            @elseif($blog->id == 54)
+                                <div class="column is-one-quarter is-grey is-flex is-fifty-eight">
+                                    <div class="arrow">
+                                        <div class="indexed-image">
+                                            <a href="{{ url('blogs', $blog->id) }}"><img src="{{ asset('featured/images/' . $blog->id. '.png') }}" class="reduction" alt="Public Transport: Photo by  Pexels "></a>
+                                        </div><!-- /.indexed-image -->
+                                        <i class="fa fa-long-arrow-left fa-2x"></i>
+                                        <h6><a href="{{ url('blogs', $blog->slug) }}">{{ $blog->title }}</a></h6>
+                                    </div><!-- /.arrow -->
+                                </div><!-- /.column is-one-quarter is-grey is-flex is-fifty-eight-->                                                                                                 
                             @endif
 
                         @endforeach
