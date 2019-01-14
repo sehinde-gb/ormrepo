4d77efc5619fea6b8af0b61082e2d7fc7776bf7c
yameste <yameste@yahoo.co.uk>
2019-01-14 08:29:10 +0000

    uploaded latest youtube video and draft post

--- resources/views/admin/blogs/index.blade.php
+++ resources/views/admin/blogs/index.blade.php
@@ -667,7 +667,18 @@
                                         <i class="fa fa-long-arrow-left fa-2x"></i>
                                         <h6><a href="{{ url('admin/blogs', $blog->id) }}">{{ $blog->title }}</a></h6>
                                     </div><!-- /.arrow -->
-                                </div><!-- /.column is-one-quarter is-grey is-flex is-sixty-three-->          
+                                </div><!-- /.column is-one-quarter is-grey is-flex is-sixty-three--> 
+
+                            @elseif($blog->id == 60)
+                                <div class="column is-one-quarter is-dark-grey is-flex is-sixty-four">
+                                    <div class="arrow">
+                                        <div class="indexed-image">
+                                            <a href="{{ url('admin/blogs', $blog->id) }}"><img src="{{ asset('featured/images/' . $blog->id. '.png') }}" class="reduction" alt="Public Transport:  Photo by Pexels "></a>
+                                        </div>
+                                        <i class="fa fa-long-arrow-left fa-2x"></i>
+                                        <h6><a href="{{ url('admin/blogs', $blog->id) }}">{{ $blog->title }}</a></h6>
+                                    </div><!-- /.arrow -->
+                                </div><!-- /.column is-one-quarter is-dark-grey is-flex is-sixty-four-->                       
                             @endif
 
                         @endforeach
--- resources/views/pages/diversecodertv.blade.php
+++ resources/views/pages/diversecodertv.blade.php
@@ -11,10 +11,45 @@
     <div class="main-container">
         <h7 id="up"></h7>
         <div class="video-container">
+                     
+
+
             <div class="diverse-container">
                 <img srcset="/images/assets/coderlogo-desktop.png 601w,
                         /images/assets/coderlogo-tall.png 600w"
                      alt="We help you to learn web development and the blockchain">
+
+<div class="diverse-container">
+                                <img srcset="/images/assets/ageoftrain-desktop.png 601w,
+                                /images/assets/ageoftrain-tall.png 600w"
+                                alt="A picture of myself at St Margarets Station">
+                        </div><!-- /.diverse-container -->    
+
+                                <div class="cliche-container">
+                                    <div class="opinion is--centered">
+                                        <div class="align-icon">
+                                            <div class="cliche">
+                                                <img src="/images/assets/train.png" class="down" alt="A picture of an underground train">
+                                            </div><!-- /.cliche -->
+                                        </div><!-- /.align-icon -->
+                                        <h17>"This is the Age Of The Train"</h17>
+                                    </div><!-- /.opinion -->
+
+                                   
+                                </div><!-- /.cliche-container -->
+
+                                <div class="composite-container">
+                                        <div class="single-container">
+                                            <h5 class="is--white is--capitals is--padded-b10">What Is My Manifesto ?</h5>
+                                            <iframe width="560" height="315" src="https://www.youtube.com/embed/rt1lxcYoWxE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div><!-- /.single-container -->
+    
+                                        <div class="single-container">
+                                           
+                                        </div><!-- /.single-container -->
+                                </div><!-- /.composite-container -->    
+
+
                         <div class="cliche-container">
                             <div class="opinion is--centered">
                                 <div class="align-icon">
@@ -38,6 +73,10 @@
                         </div><!-- /.cliche-container -->
 
             </div><!-- /.diverse-container -->
+
+
+
+
                             <div class="diverse-container">
                                     <img srcset="/images/assets/learning-desktop.png 601w,
                                     /images/assets/learning-tall.png 600w"
