3a884b4c249479ea1b514853c4894bcdd0709300
yameste <yameste@yahoo.co.uk>
2019-04-13 12:00:40 +0100

    reverted

--- resources/views/admin/blogs/index.blade.php
+++ resources/views/admin/blogs/index.blade.php
@@ -17,8 +17,7 @@
                     <div class="intro-slider">
                         <div class="collection">
                             <div class="logo-header">
-                                <h8>CHAIN</h8>BLOG</h9>
-                                
+                                <h8>CHAIN</h8><h9><span class="underlined">B</span>LOG</h9>
                                 <div class="statement">
 
                                 </div><!-- /.statement -->
