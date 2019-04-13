1ca610163fefa65175ced4ac4fd1bd0118804560
yameste <yameste@yahoo.co.uk>
2019-04-13 12:23:04 +0100

    updated copy

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -15,6 +15,8 @@
                         <div class="collection">
                             <div class="logo-header">
                                 <h8>THE CHAIN</h8><h9><span class="underlined">B</span>LOG</h9>
+
+                                <h5 class="is--beige text-transform">Trustworthy, Captivating Ways To Boost Your Supply Chain</h5>
                                 <div class="statement">
 
                                 </div><!-- /.statement -->
