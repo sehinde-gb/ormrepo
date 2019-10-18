d2817513c4af0b885119dc163bc2ae23e1ca1290
yameste <yameste@yahoo.co.uk>
2019-10-18 08:31:23 +0100

    uploaded latest youtube video

--- resources/views/pages/highlights.blade.php
+++ resources/views/pages/highlights.blade.php
@@ -15,8 +15,8 @@
         <a href="/"><button type="submit" class="btn btn-highlights">Home</button></a>
 
         <a href="https://youtu.be/-iilpXv_07A"><button type="submit" class="btn btn-highlights">Successful Guerilla Marketing Pt. 1</button></a>
-        <a href="https://youtu.be/7I0fQnx-l0M"><button type="submit" class="btn btn-highlights">Successful Guerilla Marketing Pt. 2</button></a>"><button type="submit" class="btn btn-highlights">Successful Guerilla Marketing Pt. 1</button></a>
-  
+        <a href="https://youtu.be/7I0fQnx-l0M"><button type="submit" class="btn btn-highlights">Successful Guerilla Marketing Pt. 2</button></a>
+        <a href=" https://youtu.be/aPnVuZQe9kE"><button type="submit" class="btn btn-highlights">How To Turn Your CHOSEN NICHE In To A Success</button></a>
 </div><!-- /.coder-container -->
 
 @endsection
