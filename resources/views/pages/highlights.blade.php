531fc2bacb5f28b11268c9bb87a22bb07a429638
yameste <yameste@yahoo.co.uk>
2019-10-18 15:57:18 +0100

    update

--- resources/views/pages/highlights.blade.php
+++ resources/views/pages/highlights.blade.php
@@ -16,7 +16,7 @@
 
         <a href="https://youtu.be/-iilpXv_07A"><button type="submit" class="btn btn-highlights">Successful Guerilla Marketing Pt. 1</button></a>
         <a href="https://youtu.be/7I0fQnx-l0M"><button type="submit" class="btn btn-highlights">Successful Guerilla Marketing Pt. 2</button></a>
-        <a href=" https://youtu.be/aPnVuZQe9kE"><button type="submit" class="btn btn-highlights">How To Turn Your CHOSEN NICHE In To A Success</button></a>
+        <a href=" https://youtu.be/aPnVuZQe9kE"><button type="submit" class="btn btn-highlights">Stand Out Niche !</button></a>
 </div><!-- /.coder-container -->
 
 @endsection
